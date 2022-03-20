a83c9c861d90a54691e036111731b0f5bb9a26a9
unknown <goshabrajenko@gmail.com>
2022-03-20 14:17:17 +0300

    1

--- app.py
+++ app.py
@@ -1,14 +1,49 @@
+from email.mime import image
+from tkinter import image_names
 from flask import Flask, render_template, redirect
-from city_images import get_image_of_city
+from city_images import CityChecker
 import random
 
-app = Flask(__name__)
-cities = ['Москва', 'Санкт-Петербург']
+from flask_wtf import FlaskForm
+from wtforms import *
+from wtforms.validators import DataRequired
 
-@app.route('/')
+
+class GuessCityForm(FlaskForm):
+    city_name = StringField('Название города', validators=[DataRequired()])
+    submit = SubmitField('Подтвердить')
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '0'
+checker = CityChecker()
+
+
+@app.route('/',  methods=['GET', 'POST'])
 def index():
-    img = get_image_of_city(random.choice(cities))
-    return render_template('index.html', image_name=img)
+    form = GuessCityForm()
+    fail = False
+
+    if form.validate_on_submit():
+        print(form.city_name.data)
+        if checker.check_city(form.city_name.data):
+            city, img = checker.get_new_city()
+            
+            if not city:
+                checker.restart()
+                
+                return render_template('fin.html')
+            
+        else:
+            fail = True
+            city, img = checker.get_curr_city()
+    
+    else: 
+        city, img = checker.get_curr_city()
+        print(city, img)
+
+    return render_template('index.html', image_name=img, fail=fail, form=form)
+
 
 @app.route('/all_cities')
 def all_cities():
--- city_images.py
+++ city_images.py
@@ -1,49 +1,75 @@
 import requests
 import os
+import random
+ 
 
-
-
-def get_image_of_city(city_name):
-
-    # if already_have(city_name):
-    #     print(1)
-    #     return f'{city_name}.jpg'
-
-    static_server = 'http://static-maps.yandex.ru/1.x/'
-    geocode_server = 'http://geocode-maps.yandex.ru/1.x/'
-
-    geocoder_params = {
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": city_name,
-        "format": "json",
-        "results": 1
-    }
-
-    geocode_response = requests.get(geocode_server, params=geocoder_params).json()
-    # на всякий случай bbox
-    # bbox = geocode_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['boundedBy']['Envelope']
-    ll = geocode_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+class CityChecker:
+    def __init__(self):
+        self.cities_left = ['Москва', 'Санкт-Петербург', 'Казань', 'Севастополь', 'Нижний Новгород']
+        self.curr = random.choice(self.cities_left)
+        self.cities_left.remove(self.curr)
+        self.first = True
     
+    def check_city(self, city):
+        return self.curr.lower() == city.lower()
     
-    map_params = {
-        "ll": ",".join(ll.split(' ')),
-        # "bbox": ",".join(bbox['lowerCorner'].split(' ')) + "~" + ",".join(bbox['upperCorner'].split(' ')),
-        "z": 13,
-        "size": "450,450",
-        "l": "map"
-    }
+    def get_new_city(self):
 
-    map_response = requests.get(static_server, params=map_params)
-    print(map_response)
-
-    with open(f'./static/img/{city_name}.jpg', mode='wb') as f:
-        f.write(map_response.content)
+        try:
+            self.curr = random.choice(self.cities_left)
+            self.cities_left.remove(self.curr)
+        except IndexError:
+            return None, None
+        
+        return self.curr, self._get_image_of_city(self.curr)
     
-    return f'{city_name}.jpg'
+    def get_curr_city(self):
+        return self.curr, self._get_image_of_city(self.curr)
+        
+    
+    def _get_image_of_city(self, city_name):
+
+        if self._already_have(city_name):
+            print(1)
+            return f'{city_name}.jpg'
+
+        static_server = 'http://static-maps.yandex.ru/1.x/'
+        geocode_server = 'http://geocode-maps.yandex.ru/1.x/'
+
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": city_name,
+            "format": "json",
+            "results": 1
+        }
+
+        geocode_response = requests.get(geocode_server, params=geocoder_params).json()
+        # на всякий случай bbox
+        # bbox = geocode_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['boundedBy']['Envelope']
+        ll = geocode_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+        
+        
+        map_params = {
+            "ll": ",".join(ll.split(' ')),
+            # "bbox": ",".join(bbox['lowerCorner'].split(' ')) + "~" + ",".join(bbox['upperCorner'].split(' ')),
+            "z": 13,
+            "size": "450,450",
+            "l": "map"
+        }
+
+        map_response = requests.get(static_server, params=map_params)
+
+        with open(f'./static/img/{city_name}.jpg', mode='wb') as f:
+            f.write(map_response.content)
+        
+        return f'{city_name}.jpg'
 
 
-def already_have(city_name):
-    return f'{city_name}.jpg' in os.listdir('./static/img')
-
-
-get_image_of_city('Санкт-петербург')
+    def _already_have(self, city_name):
+        return f'{city_name}.jpg' in os.listdir('./static/img')
+    
+    def restart(self):
+        self.cities_left = ['Москва', 'Санкт-Петербург', 'Казань', 'Севастополь']
+        self.curr = random.choice(self.cities_left)
+        self.cities_left.remove(self.curr)
+        self.first = True
